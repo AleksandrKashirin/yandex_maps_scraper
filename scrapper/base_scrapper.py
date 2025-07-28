2bd00655290716ec37661c0b53a13ad06a551edc
Aleksandr Kashirin <kashirin.aleks@ya.ru>
2025-07-28 19:59:25 +0300

    Initial commit

---
+++ scrapper/base_scrapper.py
@@ -0,0 +1,8 @@
+class YandexMapsScraper:
+    def __init__(self, config)
+    def load_page(self, url)
+    def extract_basic_info(self)
+    def navigate_to_services_tab(self)
+    def navigate_to_reviews_tab(self)
+    def extract_services(self)
+    def extract_reviews(self)
