a46c43a177ac9c0f349bcd79bf5a4e9f30947211
Aleksandr Kashirin <kashirin.aleks@ya.ru>
2025-07-30 11:33:26 +0300

    gitignore

--- config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Пакет конфигурации для системы скрапинга Яндекс.Карт
-"""
-
-from .chrome_config import ChromeConfig, user_agent_rotator
-from .settings import settings
-
-__all__ = ["settings", "ChromeConfig", "user_agent_rotator"]
--- core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Ядро системы скрапинга
-"""
-
-from .logger import get_logger, log_scraping_stats, scraping_metrics
-
-__all__ = ["get_logger", "log_scraping_stats", "scraping_metrics"]
